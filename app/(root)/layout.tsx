0c396e4fc23c1d1eba4f06254e5106705d480086
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-06-22 19:10:27 +0700

    Add sidebars

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,28 @@
+import Image from 'next/image'
+
+import SideBar from '@/components/SideBar'
+import MobileNav from '@/components/MobileNav'
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const loggedIn = { firstName: 'John', lastName: 'Smith' }
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className='flex h-screen w-full font-inter'>
+      <SideBar user={loggedIn} />
+
+      <div className='flex size-full flex-col'>
+        <div className='root-layout'>
+          <Image src='/icons/logo.svg' width={30} height={30} alt='logo' />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 export default function Home() {
-  const loggedIn = { firstName: 'Adrian' }
+  const loggedIn = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'example@gmail.com',
+  }
 
   return (
     <section className='home'>
@@ -22,7 +27,13 @@ export default function Home() {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   )
 }
