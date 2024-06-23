9fca60ca589ad9b0fca6d8f7a423922e9e9674fb
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-06-23 08:38:44 +0700

    Create auth page UI

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,10 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 export default function SignUp() {
-  return <div>SignUp</div>
+  return (
+    <section className='flex-center size-full max-sm:px-6'>
+      <AuthForm type='sign-up' />
+    </section>
+  )
 }
